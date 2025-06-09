53048d5f75bc40cc185a3cb14cdba5bcf7700ca2
Harsha Reddy T <hvr.thotapalli@outlook.com>
2025-06-09 19:59:11 +0530

    Update SectionCards component to reflect email processing metrics and improve layout

--- src/components/section-cards.tsx
+++ src/components/section-cards.tsx
@@ -12,34 +12,29 @@ import {
 
 export function SectionCards() {
   return (
-    <div className="*:data-[slot=card]:from-primary/5 *:data-[slot=card]:to-card dark:*:data-[slot=card]:bg-card grid grid-cols-1 gap-4 px-4 *:data-[slot=card]:bg-gradient-to-t *:data-[slot=card]:shadow-xs lg:px-6 @xl/main:grid-cols-2 @5xl/main:grid-cols-4">
+    <div className="*:data-[slot=card]:from-primary/5 *:data-[slot=card]:to-card dark:*:data-[slot=card]:bg-card grid grid-cols-2 gap-4 px-4 *:data-[slot=card]:bg-gradient-to-t *:data-[slot=card]:shadow-xs lg:px-6 @xl/main:grid-cols-6 @5xl/main:grid-cols-6">
       <Card className="@container/card bg-gray-100 dark:bg-gray-900 hover:bg-gray-200 hover:dark:bg-gray-800 border-b-gray-500 border-b-4">
         <CardHeader>
-          <CardDescription>Total Success</CardDescription>
-          <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
-            $1,250.00
+          <CardDescription>Total Emails</CardDescription>
+          <CardTitle className="text-3xl font-bold tabular-nums @[250px]/card:text-3xl">
+            43
           </CardTitle>
           <CardAction>
             <Badge className="bg-gray-500 text-white" variant="default">
               <IconTrendingUp />
-              +12.5%
+              12.5%
             </Badge>
           </CardAction>
         </CardHeader>
         <CardFooter className="flex-col items-start gap-1.5 text-sm">
-          <div className="line-clamp-1 flex gap-2 font-medium">
-            Trending up this month <IconTrendingUp className="size-4" />
-          </div>
-          <div className="text-muted-foreground">
-            Visitors for the last 6 months
-          </div>
+          <div className="text-muted-foreground">Total emails Processed</div>
         </CardFooter>
       </Card>
       <Card className="@container/card bg-red-100 dark:bg-red-900 hover:bg-red-200 hover:dark:bg-red-800 border-b-red-500 border-b-4">
         <CardHeader>
           <CardDescription>Total Failures</CardDescription>
-          <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
-            1,234
+          <CardTitle className="text-3xl font-bold tabular-nums @[250px]/card:text-3xl">
+            50
           </CardTitle>
           <CardAction>
             <Badge className="bg-red-500 text-white" variant="default">
@@ -49,39 +44,35 @@ export function SectionCards() {
           </CardAction>
         </CardHeader>
         <CardFooter className="flex-col items-start gap-1.5 text-sm">
-          <div className="line-clamp-1 flex gap-2 font-medium">
-            Down 20% this period <IconTrendingDown className="size-4" />
-          </div>
           <div className="text-muted-foreground">
-            Acquisition needs attention
+            Completely Failed Invoices
           </div>
         </CardFooter>
       </Card>
       <Card className="@container/card bg-amber-100 dark:bg-amber-900 hover:bg-amber-200 hover:dark:bg-amber-800 border-b-amber-500 border-b-4">
         <CardHeader>
           <CardDescription>Partial Success</CardDescription>
-          <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
-            45,678
+          <CardTitle className="text-3xl font-bold tabular-nums @[250px]/card:text-3xl">
+            5
           </CardTitle>
           <CardAction>
             <Badge className="bg-amber-500 text-white" variant="default">
               <IconTrendingUp />
-              +12.5%
+              02.5%
             </Badge>
           </CardAction>
         </CardHeader>
         <CardFooter className="flex-col items-start gap-1.5 text-sm">
-          <div className="line-clamp-1 flex gap-2 font-medium">
-            Strong user retention <IconTrendingUp className="size-4" />
+          <div className="text-muted-foreground">
+            Partially Successful Invoices
           </div>
-          <div className="text-muted-foreground">Engagement exceed targets</div>
         </CardFooter>
       </Card>
       <Card className="@container/card bg-blue-100 dark:bg-blue-900 hover:bg-blue-200 hover:dark:bg-blue-800 border-b-blue-500 border-b-4">
         <CardHeader>
           <CardDescription>Overall Files</CardDescription>
-          <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
-            4.5%
+          <CardTitle className="text-3xl font-bold tabular-nums @[250px]/card:text-3xl">
+            110
           </CardTitle>
           <CardAction>
             <Badge className="bg-blue-200 text-black" variant="default">
@@ -91,17 +82,14 @@ export function SectionCards() {
           </CardAction>
         </CardHeader>
         <CardFooter className="flex-col items-start gap-1.5 text-sm">
-          <div className="line-clamp-1 flex gap-2 font-medium">
-            Steady performance increase <IconTrendingUp className="size-4" />
-          </div>
-          <div className="text-muted-foreground">Meets growth projections</div>
+          <div className="text-muted-foreground">All Invoices Processed</div>
         </CardFooter>
       </Card>
       <Card className="@container/card bg-green-100 dark:bg-green-900 hover:bg-green-200 hover:dark:bg-green-800 border-b-green-500 border-b-4">
         <CardHeader>
           <CardDescription>Total Success</CardDescription>
-          <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
-            $1,250.00
+          <CardTitle className="text-3xl font-bold tabular-nums @[250px]/card:text-3xl">
+            50
           </CardTitle>
           <CardAction>
             <Badge className="bg-green-500 text-white" variant="default">
@@ -111,19 +99,16 @@ export function SectionCards() {
           </CardAction>
         </CardHeader>
         <CardFooter className="flex-col items-start gap-1.5 text-sm">
-          <div className="line-clamp-1 flex gap-2 font-medium">
-            Trending up this month <IconTrendingUp className="size-4" />
-          </div>
           <div className="text-muted-foreground">
-            Visitors for the last 6 months
+            End to End Successful Invoices
           </div>
         </CardFooter>
       </Card>
       <Card className="@container/card bg-violet-100 dark:bg-violet-900 hover:bg-violet-200 hover:dark:bg-violet-800 border-b-violet-500 border-b-4">
         <CardHeader>
-          <CardDescription>Total Failures</CardDescription>
-          <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
-            1,234
+          <CardDescription>Missing Info</CardDescription>
+          <CardTitle className="text-3xl font-bold tabular-nums @[250px]/card:text-3xl">
+            5
           </CardTitle>
           <CardAction>
             <Badge className="bg-violet-500 text-white" variant="default">
@@ -132,53 +117,8 @@ export function SectionCards() {
             </Badge>
           </CardAction>
         </CardHeader>
-        <CardFooter className="flex-col items-start gap-1.5 text-sm">
-          <div className="line-clamp-1 flex gap-2 font-medium">
-            Down 20% this period <IconTrendingDown className="size-4" />
-          </div>
-          <div className="text-muted-foreground">
-            Acquisition needs attention
-          </div>
-        </CardFooter>
-      </Card>
-      <Card className="@container/card bg-cyan-100 dark:bg-cyan-900 hover:bg-cyan-200 hover:dark:bg-cyan-800 border-b-cyan-500 border-b-4">
-        <CardHeader>
-          <CardDescription>Partial Success</CardDescription>
-          <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
-            45,678
-          </CardTitle>
-          <CardAction>
-            <Badge className="bg-cyan-500 text-white" variant="default">
-              <IconTrendingUp />
-              +12.5%
-            </Badge>
-          </CardAction>
-        </CardHeader>
-        <CardFooter className="flex-col items-start gap-1.5 text-sm">
-          <div className="line-clamp-1 flex gap-2 font-medium">
-            Strong user retention <IconTrendingUp className="size-4" />
-          </div>
-          <div className="text-muted-foreground">Engagement exceed targets</div>
-        </CardFooter>
-      </Card>
-      <Card className="@container/card bg-pink-100 dark:bg-pink-900 hover:bg-pink-200 hover:dark:bg-pink-800 border-b-pink-500 border-b-4">
-        <CardHeader>
-          <CardDescription>Overall Files</CardDescription>
-          <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
-            4.5%
-          </CardTitle>
-          <CardAction>
-            <Badge className="bg-pink-200 text-black" variant="default">
-              <IconTrendingUp />
-              +4.5%
-            </Badge>
-          </CardAction>
-        </CardHeader>
-        <CardFooter className="flex-col items-start gap-1.5 text-sm">
-          <div className="line-clamp-1 flex gap-2 font-medium">
-            Steady performance increase <IconTrendingUp className="size-4" />
-          </div>
-          <div className="text-muted-foreground">Meets growth projections</div>
+        <CardFooter className="flex-col items-start gap-1.5 text-md">
+          <div className="text-muted-foreground">Missing Information</div>
         </CardFooter>
       </Card>
     </div>
